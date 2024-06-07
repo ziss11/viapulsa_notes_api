a9c30d8b1514a6fe6248cd00710d2fb904d40a78
Abdul Azis <abdul.azizz1108@gmail.com>
2024-06-07 19:46:43 +0700

    .

--- api/index.js
+++ api/index.js
@@ -3,56 +3,52 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/notes', async (req, res) => {
+    const notes = await prisma.notes.findMany();
+    res.status(200).json({
+        status: 'success',
+        data: notes,
+    });
 });
 
-// app.get('/notes', async (req, res) => {
-//     const notes = await prisma.notes.findMany();
-//     res.status(200).json({
-//         status: 'success',
-//         data: notes,
-//     });
-// });
+app.post('/notes', async (req, res) => {
+    const note = await prisma.notes.create({
+        data: req.body,
+    });
+    res.status(201).json({
+        status: 'success',
+        message: 'Catatan berhasil ditambahkan',
+        data: note,
+    });
+});
 
-// app.post('/notes', async (req, res) => {
-//     const note = await prisma.notes.create({
-//         data: req.body,
-//     });
-//     res.status(201).json({
-//         status: 'success',
-//         message: 'Catatan berhasil ditambahkan',
-//         data: note,
-//     });
-// });
+app.patch('/notes/:id', async (req, res) => {
+    const { id } = req.params;
+    const note = await prisma.notes.update({
+        where: { id },
+        data: req.body,
+    });
+    res.status(200).json({
+        status: 'success',
+        message: 'Catatan berhasil diubah',
+        data: note,
+    });
+});
 
-// app.patch('/notes/:id', async (req, res) => {
-//     const { id } = req.params;
-//     const note = await prisma.notes.update({
-//         where: { id },
-//         data: req.body,
-//     });
-//     res.status(200).json({
-//         status: 'success',
-//         message: 'Catatan berhasil diubah',
-//         data: note,
-//     });
-// });
-
-// app.delete('/notes/:id', async (req, res) => {
-//     const { id } = req.params;
-//     await prisma.notes.delete({
-//         where: { id },
-//     });
-//     res.status(200).json({
-//         status: 'success',
-//         message: 'Catatan berhasil dihapus',
-//     });
-// });
+app.delete('/notes/:id', async (req, res) => {
+    const { id } = req.params;
+    await prisma.notes.delete({
+        where: { id },
+    });
+    res.status(200).json({
+        status: 'success',
+        message: 'Catatan berhasil dihapus',
+    });
+});
 
 app.listen(port);
 
